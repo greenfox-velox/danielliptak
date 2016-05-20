58d63185c1d0b737217e1382061c915e1c3b4e7b
danielliptak <daniliptak@gmail.com>
2016-05-20 07:17:25 +0200

    6.py and 7.py excercises have done as homework

---
+++ week3/day3/6.py
@@ -0,0 +1,5 @@
+def reverse(input):
+    new_list = input[::-1]
+    return new_list
+
+print(reverse([2, 4, 6, 11, 9]))
---
+++ week3/day3/7.py
@@ -0,0 +1,7 @@
+def double(input):
+    new_list = []
+    for item in input:
+        new_list.append(item * 2)
+    return new_list
+
+print(double([2, 4, 6, 11, 9]))
