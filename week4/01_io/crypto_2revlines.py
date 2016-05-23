746af91456247e6495bac2b49f6d27f65eb7a204
danielliptak <daniliptak@gmail.com>
2016-05-23 23:02:06 +0200

    correct my last method

--- week4/01_io/crypto_2revlines.py
+++ week4/01_io/crypto_2revlines.py
@@ -1,9 +1,9 @@
 # Create a method that decrypts texts/reversed_zen_lines.txt
 def decrypt(file_name):
-    txt = open('texts/reversed_zen_lines.txt')
-    raw_text = txt.readlines()
-    my_text = ''
-    for line in raw_text:
-        my_text += line[-2::-1] + '\n'
-    txt.close()
-    return my_text
+  txt = open('texts/reversed_zen_lines.txt')
+  raw_text = txt.readlines()
+  my_text = ''
+  for line in raw_text:
+    my_text += line[-2::-1] + '\n'
+  txt.close()
+  return my_text
--- week4/01_io/crypto_4encoded.py
+++ week4/01_io/crypto_4encoded.py
@@ -1,12 +1,17 @@
 # Create a method that decrypts texts/encoded_zen_lines.txt
-def decrypts(file_name):
+def decrypt(file_name):
   txt = open('texts/encoded_zen_lines.txt')
   raw_text = txt.read()
   my_text = ''
   translate = []
   for letter in raw_text:
-    if not letter == ' ' or '\n':
-      translate.append(ord(letter)-1)
+    if letter == ' ':
+      translate.append(ord(letter))
+    elif letter == '\n':
+      translate.append(ord(letter))
     else:
-      translate.append(ord(letter)
-decrypts('texts/encoded_zen_lines.txt')
+      translate.append(ord(letter)-1)
+  for num in translate:
+    my_text += chr(num)
+  txt.close()
+  return (my_text)
