ea88757f0f0a11f4bb44d7e43f14a40a958ee78e
danielliptak <daniliptak@gmail.com>
2016-06-22 21:41:35 +0200

    fix fight

--- week6/Character.py
+++ week6/Character.py
@@ -2,24 +2,29 @@ from tkinter import *
 from random import randint
 
 class Character(object):
-    def __init__(self, x, y, canvas, max_hp, DP, SP):
+    def __init__(self, x, y, canvas, max_hp, DP, SP, level):
         self.y = y
         self.x = x
         self.canvas = canvas
         self.max_hp = max_hp
         self.dp = DP
         self.sp = SP
+        self.level = 1
+        print (self.level)
     def draw_character(self, pic):
         self.canvas.create_image(self.x * 72, self.y * 72, anchor=NW, image=pic)
+    def attak(self, enemy):
+        enemy.current_hp -= self.sp
+
 
 class Hero(Character):
-    def __init__(self, canvas, pic):
+    def __init__(self, canvas, level, pic):
         x = 0
         y = 0
         DP = 60
         SP = 40
         max_hp = 100
-        super().__init__(x, y, canvas, max_hp, DP, SP)
+        super().__init__(x, y, canvas, max_hp, DP, SP, level)
         self.draw_character(pic)
         self.current_hp = self.max_hp
 
@@ -38,20 +43,27 @@ class Hero(Character):
             self.draw_character(pic)
 
     def __str__(self):
-            return 'Hero     (Level{})     HP: {}/{} |   DP:{}   |   SP: {}'.format(1,self.max_hp, self.current_hp, self.dp, self.sp)
+        return 'Hero     (Level{})     HP: {}/{} |   DP:{}   |   SP: {}'.format(self.level, self.max_hp, self.current_hp, self.dp, self.sp)
 
 class Skeleton(Character):
-    def __init__(self,x ,y, canvas, max_hp, DP, SP, skeleton):
-        super().__init__(x, y, canvas, max_hp, DP, SP)
+    def __init__(self,x ,y, canvas, max_hp, DP, SP, level, skeleton):
+        super().__init__(x, y, canvas, max_hp, DP, SP, level)
         self.skeleton = skeleton
         self.draw_character(self.skeleton)
         self.x = x
         self.y = y
+        self.current_hp = self.max_hp
+
+    def __str__(self):
+        return 'Skeleton     (Level{})     HP: {}/{} |   DP:{}   |   SP: {}'.format(self.level,self.max_hp, self.current_hp, self.dp, self.sp)
 
 class Boss(Character):
-    def __init__(self, x, y, canvas, max_hp, DP, SP, boss):
-        super().__init__(x, y, canvas, max_hp, DP, SP)
+    def __init__(self, x, y, canvas, max_hp, DP, SP, level, boss):
+        super().__init__(x, y, canvas, max_hp, DP, SP, level)
         self.boss = boss
         self.draw_character(self.boss)
         self.x = x
         self.y = y
+
+    def __str__(self):
+        return 'Boss     (Level{})     HP: {}/{} |   DP:{}   |   SP: {}'.format(self.level,self.max_hp, self.current_hp, self.dp, self.sp)
--- week6/game.py
+++ week6/game.py
@@ -15,6 +15,7 @@ class Game():
         self.skeleton = PhotoImage(file='skeleton.png')
         self.boss = PhotoImage(file='boss.png')
         self.canvas = canvas
+        self.level = 1
         self.map1 = [[0,0,1,0,0,0,0,0,0,0],
                 [0,0,1,0,0,1,1,1,0,1],
                 [0,1,0,0,0,1,1,1,0,1],
@@ -26,15 +27,15 @@ class Game():
                 [0,1,0,0,0,0,0,0,0,0],
                 [0,0,0,0,0,1,1,1,0,1]]
         self.level = Board(self.canvas, self.map1, self.wall, self.floor)
-        self.Aladin = Hero(self.canvas, self.hero_down)
+        self.Aladin = Hero(self.canvas, self.level, self.hero_down)
         self.skeleton1_id = self.random_gen()
-        self.skeleton1 = Skeleton(self.skeleton1_id[1], self.skeleton1_id[0], self.canvas,100, 40, 60, self.skeleton)
+        self.skeleton1 = Skeleton(self.skeleton1_id[1], self.skeleton1_id[0], self.canvas,100, 40, 60, self.level, self.skeleton)
         self.skeleton2_id = self.random_gen()
-        self.skeleton2 = Skeleton(self.skeleton2_id[1], self.skeleton2_id[0], self.canvas,100, 40, 60, self.skeleton)
+        self.skeleton2 = Skeleton(self.skeleton2_id[1], self.skeleton2_id[0], self.canvas,100, 40, 60, self.level ,self.skeleton)
         self.skeleton3_id = self.random_gen()
-        self.skeleton3 = Skeleton(self.skeleton3_id[1], self.skeleton3_id[0], self.canvas,100, 40, 60, self.skeleton)
+        self.skeleton3 = Skeleton(self.skeleton3_id[1], self.skeleton3_id[0], self.canvas,100, 40, 60, self.level ,self.skeleton)
         self.boss_id = self.random_gen()
-        self.bigboss = Boss(self.boss_id[1], self.boss_id[0], self.canvas,100, 40, 60, self.boss)
+        self.bigboss = Boss(self.boss_id[1], self.boss_id[0], self.canvas,100, 40, 60, self.level, self.boss)
         self.status = self.Aladin.__str__()
         self.draw_status_bar()
 
@@ -52,7 +53,7 @@ class Game():
         self.skeleton3.draw_character(self.skeleton)
         self.bigboss.draw_character(self.boss)
         self.move_hero(key)
-        self.check_position()
+        self.battle(key)
 
     def move_hero(self, key):
         if key.keysym == 'Down':
@@ -79,10 +80,30 @@ class Game():
             else:
                 self.Aladin.draw_character(self.hero_left)
 
-    def check_position(self):
-        if self.bigboss.x == self.Aladin.x and self.bigboss.y == self.Aladin.y:
-            pass
-
     def draw_status_bar(self):
+        self.canvas.create_text(20, 723, text= self.status, justify=RIGHT, tags="status", anchor=NW, font='arial')
+
+    def draw_enemy_status_bar(self, type_of_enemy):
         self.canvas.delete("status")
-        self.canvas.create_text(60, 723, text= self.status, justify=RIGHT, tags="status", anchor=NW, font='arial')
+        self.draw_status_bar()
+        self.canvas.create_text(400 , 723, text= type_of_enemy.__str__(), justify=LEFT, tags="status", anchor=NW, font='arial')
+
+    def battle(self, key):
+        if self.skeleton1_id[1] == self.Aladin.x and self.skeleton1_id[0] == self.Aladin.y:
+            self.draw_enemy_status_bar(self.skeleton1)
+            if key.keysym == 'Escape':
+                self.Aladin.attak(self.skeleton1)
+                self.Aladin.draw_character(self.hero_down)
+                self.draw_enemy_status_bar(self.skeleton1)
+        elif self.skeleton2_id[1] == self.Aladin.x and self.skeleton2_id[0] == self.Aladin.y:
+            self.draw_enemy_status_bar(self.skeleton2)
+            if key.keysym == 'Escape':
+                self.Aladin.attak(self.skeleton2)
+                self.Aladin.draw_character(self.hero_down)
+                self.draw_enemy_status_bar(self.skeleton2)
+        elif self.skeleton3_id[1] == self.Aladin.x and self.skeleton3_id[0] == self.Aladin.y:
+            self.draw_enemy_status_bar(self.skeleton3)
+            if key.keysym == 'Escape':
+                self.Aladin.attak(self.skeleton3)
+                self.Aladin.draw_character(self.hero_down)
+                self.draw_enemy_status_bar(self.skeleton3)
