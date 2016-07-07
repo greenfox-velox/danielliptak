2820dd8138548e07e37ccec938ba9470c9c1960a
danielliptak <daniliptak@gmail.com>
2016-07-07 15:31:20 +0200

    fix add HTTPRequest

--- week8/ToDo/todo.js
+++ week8/ToDo/todo.js
@@ -2,16 +2,24 @@
   // var sentence = document.querySelector('.rawText').value
 var xhr = new XMLHttpRequest();
 xhr.onload = function () {
-  var todoList = JSON.parse(xhr.response)
+  var todoList = JSON.parse(xhr.response);
   todoList.forEach(function(item){
     var newItem = document.createElement('li');
     newItem.textContent = item['text'];
-    document.querySelector('ul').appendChild(new_item);
-  })
+    document.querySelector('ul').appendChild(newItem);
+  });
 };
 xhr.open('GET', 'https://mysterious-dusk-8248.herokuapp.com/todos', true);
 xhr.send();
 
 
+
+function add() {
+  var xhr2 = new XMLHttpRequest();
+  xhr2.open('POST', 'https://mysterious-dusk-8248.herokuapp.com/todos', true);
+  xhr2.setRequestHeader('Content-Type', 'application/json');
+  xhr2.send(JSON.stringify({ text: 'Kill John Rambo' }));
+}
+
 var button = document.querySelector('button');
-//button.addEventListener('click', Translate);
+button.addEventListener('click', add);
